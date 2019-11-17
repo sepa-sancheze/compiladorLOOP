317c0aa551d829a7b62e2bf430b43fa30bdd44de
Gilda <gmrp22@hotmail.com>
2019-11-17 15:14:31 -0600

    escribir simbolos

---
+++ compLOOP/src/sumadoraarbol/EscribirenFichero.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 tito88
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package sumadoraarbol;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tito88
+ */
+public class EscribirenFichero {
+
+    public void escribir(ArrayList<Simbolos> sim) {
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try {
+            fichero = new FileWriter("C:\\Users\\tito88\\Pictures\\compiladorLOOP\\compLOOP\\simbolos.txt");
+            pw = new PrintWriter(fichero);
+
+            for (int x = 0; x < sim.size(); x++) {
+                Simbolos sim1 = sim.get(x);
+                pw.println(sim1.cantidad + "   " + sim1.getValor() );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fichero) {
+                    fichero.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+}
--- compLOOP/src/sumadoraarbol/SumadoraArbol.java
+++ compLOOP/src/sumadoraarbol/SumadoraArbol.java
@@ -1,23 +1,25 @@
 package sumadoraarbol;
 
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class SumadoraArbol {
 
     public static void main(String[] args) {
-       try {
+        try {
             AnalizadorLexico lex = new AnalizadorLexico(new FileReader("numeros.txt"));
             AnalizadorSintactico parser = new AnalizadorSintactico(lex);
             parser.parse();
-            parser.arbolSintactico.setGraphvizPath("dot");
+            parser.arbolSintactico.setGraphvizPath("C:\\Users\\tito88\\Desktop\\graphviz-2.38\\release\\bin\\dot.exe");
             parser.arbolSintactico.recorrerArbol();
             parser.arbolSintactico.recorrerArbol(Arbol.TIPO_RECORRIDO_GRAFO);
             System.out.println("Prueba");
-        } 
-        catch (java.lang.Exception el)
-        {
-            System.out.println("error encontrado: "+el.getMessage()); 
+            ArrayList <Simbolos> sim = lex.sim;
+            EscribirenFichero es = new EscribirenFichero();
+            es.escribir(sim);
+        } catch (java.lang.Exception el) {
+            System.out.println("error encontrado: " + el.getMessage());
         }
     }
-    
+
 }
